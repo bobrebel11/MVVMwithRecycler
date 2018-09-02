62b34e0e24e3ae7ef6d53f0afdc5d0f269cb3678
rebelbob11 <Hunter.11>
2018-09-02 13:14:13 -0400

    Added data binding to layout file.

--- app/src/main/java/com/emptybottle/rebelbob11/mvvmwithrecycler/MainActivity.kt
+++ app/src/main/java/com/emptybottle/rebelbob11/mvvmwithrecycler/MainActivity.kt
@@ -2,11 +2,25 @@ package com.emptybottle.rebelbob11.mvvmwithrecycler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import com.emptybottle.rebelbob11.mvvmwithrecycler.viewmodel.CategoryViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private var recyclerView: RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView) as RecyclerView
+
+        var categoryViewModel: CategoryViewModel = ViewModelProviders.of(this).get(CategoryViewModel::class.java)
+
+        categoryViewModel.getArrayList().observe(this, Observer {
+
+        })
     }
 }
--- app/src/main/java/com/emptybottle/rebelbob11/mvvmwithrecycler/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/emptybottle/rebelbob11/mvvmwithrecycler/viewmodel/CategoryViewModel.kt
@@ -1,5 +1,6 @@
 package com.emptybottle.rebelbob11.mvvmwithrecycler.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.emptybottle.rebelbob11.mvvmwithrecycler.model.Category
 
@@ -13,11 +14,35 @@ class CategoryViewModel: ViewModel
 
     constructor() : super()
 
+    //Secondary Constructor
     constructor(category: Category) : super() {
         this.id = category.id
         this.title = category.title
         this.desc = category.desc
         this.imagePath = category.imagePath
+    }
+
+    var arrayListMutableLivedata = MutableLiveData<ArrayList<CategoryViewModel>>()
+
+    var arrayList = ArrayList<CategoryViewModel>()
+
+    fun getArrayList():MutableLiveData<ArrayList<CategoryViewModel>>{
+
+        val category1 = Category("1","title 1","This is first 1","image.png")
+
+        val category2 = Category("2","title 2","This is first 2","image.png")
+
+        val categoryViewModel1: CategoryViewModel = CategoryViewModel(category1)
+        val categoryViewModel2: CategoryViewModel = CategoryViewModel(category2)
+
+        arrayList!!.add(categoryViewModel1)
+        arrayList!!.add(categoryViewModel2)
+
+        arrayListMutableLivedata.value = arrayList
+
+
+
+        return arrayListMutableLivedata
     }
 
 
